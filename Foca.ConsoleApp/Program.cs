f17a8cf37391cc09fd31cc7102ca47417caf2827
Vitor Caitano <caitanoovitor@gmail.com>
2026-04-01 15:02:55 -0300

    Acrecentando Arrey de caractere

--- Foca.ConsoleApp/Program.cs
+++ Foca.ConsoleApp/Program.cs
@@ -1,3 +1,23 @@
-﻿Console.WriteLine("-------------------------------------");
-Console.WriteLine("----------JOGO DA FORCA----------");
-Console.WriteLine("-------------------------------------");
+﻿string palavraAleatoria = "ABACATE";
+
+
+char[] letrasCorretas = new char[7];
+
+for (int contadorDeLetras = 0; contadorDeLetras < 7; contadorDeLetras++)
+{
+    letrasCorretas[contadorDeLetras] = '_';
+    Console.Write(letrasCorretas[contadorDeLetras]);
+}
+Console.ReadLine();
+
+bool jogadorAcertou = false;
+
+while (!jogadorAcertou)
+{
+    Console.Clear();
+    Console.WriteLine("Digite uma Letra: ");
+    char letra = Convert.ToChar(Console.ReadLine());
+
+    Console.WriteLine(letra);
+    Console.ReadLine();
+}
--- Foca.ConsoleApp/obj/Debug/net10.0/Foca.ConsoleApp.AssemblyInfo.cs
+++ Foca.ConsoleApp/obj/Debug/net10.0/Foca.ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Foca.ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10decb078a26d04741c024e19289429fb7fa25e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Foca.ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Foca.ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
